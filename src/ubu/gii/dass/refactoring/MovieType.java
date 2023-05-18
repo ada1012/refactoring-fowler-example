4cd1d27c45158088a90f7ec3d54d00b6c8a45583
ada1012 <ada1012@alu.ubu.es>
2023-05-18 19:09:15 +0200

    Create abstract class

---
+++ src/ubu/gii/dass/refactoring/MovieType.java
@@ -0,0 +1,16 @@
+package ubu.gii.dass.refactoring;
+
+public abstract class MovieType {
+
+	public static final int CHILDRENS = 2;
+	public static final int REGULAR = 0;
+	public static final int NEW_RELEASE = 1;
+
+	public MovieType() {
+		super();
+	}
+	
+	public abstract int getType();
+
+	public abstract double getCharge(Rental rental);
+}
